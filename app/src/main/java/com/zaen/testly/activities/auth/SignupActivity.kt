22e6d28c21b1a93d73581a98d8d1a9391baf0259
zaen323 <chouzaen2002@gmail.com>
2018-02-21 22:18:55 +0900

    commit

---
+++ app/src/main/java/com/zaen/testly/activities/auth/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.zaen.testly.activities.auth
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.zaen.testly.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
