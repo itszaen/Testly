71fda829d813f423f16f8b66a05a82557cadf5c9
zaen323 <chouzaen2002@gmail.com>
2018-04-16 13:38:51 +0900

    commit

--- app/src/main/java/com/zaen/testly/fragments/CreateFragment.kt
+++ app/src/main/java/com/zaen/testly/fragments/CreateFragment.kt
@@ -6,20 +6,16 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import butterknife.ButterKnife
 import butterknife.Unbinder
 import com.zaen.testly.R
 
-/**
- * Created by zaen on 2/25/18.
- */
-class DashboardFragment : Fragment() {
+class CreateFragment : Fragment() {
     var activity: Activity? = null
     private var unbinder: Unbinder? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?  {
-        val view = inflater.inflate(R.layout.fragment_dashboard,container,false)
+        val view = inflater.inflate(R.layout.fragment_create,container,false)
         unbinder = ButterKnife.bind(this,view)
         return view
     }
