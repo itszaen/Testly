37d85b0b5975ef22d42f919cb7a06244cdd223e6
zaen323 <chouzaen2002@gmail.com>
2018-04-21 12:07:49 +0900

    commit

--- app/src/main/java/com/zaen/testly/App.kt
+++ app/src/main/java/com/zaen/testly/App.kt
@@ -1,10 +1,12 @@
 package com.zaen.testly
 
 import android.app.Application
+import android.content.Context
 import com.mikepenz.community_material_typeface_library.CommunityMaterial
 import com.mikepenz.fontawesome_typeface_library.FontAwesome
 import com.mikepenz.google_material_typeface_library.GoogleMaterial
 import com.mikepenz.iconics.Iconics
+import com.mikepenz.iconics.context.IconicsContextWrapper
 import com.squareup.leakcanary.LeakCanary
 import com.zaen.testly.utils.preferences.Prefs
 
@@ -29,6 +31,10 @@ class App : Application(){
         }
         registerFontIcons()
         initialiseStorage()
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(IconicsContextWrapper.wrap(base))
     }
 
     private fun registerFontIcons() {
--- app/src/main/java/com/zaen/testly/fragments/CreateFragment.kt
+++ app/src/main/java/com/zaen/testly/fragments/CreateFragment.kt
@@ -3,11 +3,13 @@ package com.zaen.testly.fragments
 import android.app.Activity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.view.LayoutInflaterCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import butterknife.ButterKnife
 import butterknife.Unbinder
+import com.mikepenz.iconics.context.IconicsLayoutInflater2
 import com.zaen.testly.R
 
 class CreateFragment : Fragment() {
