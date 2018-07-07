0a43facde97f53e77b99f8b9dfc6e7102a127080
zaen323 <chouzaen2002@gmail.com>
2018-07-07 22:13:44 +0900

    commit

--- app/src/main/java/com/zaen/testly/views/cards/ElixirSelectionCardView.kt
+++ app/src/main/java/com/zaen/testly/views/cards/ElixirSelectionCardView.kt
@@ -1,9 +1,8 @@
 package com.zaen.testly.views.cards
 
 import android.content.Context
-import android.graphics.drawable.Animatable
-import android.support.v4.content.res.ResourcesCompat
 import android.widget.FrameLayout
+import android.widget.ImageView
 import com.zaen.testly.R
 import com.zaen.testly.data.SelectionCardData
 
@@ -37,11 +36,10 @@ ISelectionCardView{
 
     override fun showAnswer(){
         for ((i, optionItemLayout) in optionItemList.withIndex()){
-            val circle = optionItemLayout.findViewById<FrameLayout>(R.id.box_card_selection_option_elixir)
-            val correct = ResourcesCompat.getDrawable(context.resources, R.drawable.circle_correct, null)
+            val circle = optionItemLayout.findViewById<ImageView>(R.id.box_card_selection_option_elixir)
             if (i == card.answer){
-                circle.background = correct
-                (correct as Animatable).start()
+                circle.setImageResource(R.drawable.circle_correct)
+                //(circle.drawable as Animatable).start()
             } else {
 
             }
